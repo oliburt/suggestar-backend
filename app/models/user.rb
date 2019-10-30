d0035c4facebff8e2c847fefc1618968cedfeae4
oliburt <oliburt@gmail.com>
2019-10-30 15:05:51 +0000

    validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :email, uniqueness: true, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :password_digest, presence: true
 
     def full_name
         "#{self.first_name} #{self.last_name}"

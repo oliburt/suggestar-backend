ab400c8d9da9620966747bc11c9c5dee51b540f7
oliburt <oliburt@gmail.com>
2019-10-30 17:13:56 +0000

    test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email: 'a@a.com', first_name: "Oliver", last_name: "Burt", password: "123", password_confirmation: "123")
+
+Venue.create(name: "Flatiron", description: "Coding School", user_id: 1)
+
+Address.create(venue_id: 1, address_identifier: "WeWork", street_number: 131, street_name: "Finsbury Pavement", address_type: "Office", governing_district: "London", minor_municipality: "Finsbury", major_municipality: "London", post_code: "EC2A 1NT", country: "United Kingdom")

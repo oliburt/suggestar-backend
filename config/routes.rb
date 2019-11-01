120f04e63184a20b44f256bee585e800ddeaa67f
oliburt <oliburt@gmail.com>
2019-11-01 13:31:54 +0000

    added user update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index]
+      resources :users, only: [:index, :update]
       resources :venues, only: [:create, :show, :index]
       post '/login', to: 'auth#create'
       post '/register', to: 'users#create'

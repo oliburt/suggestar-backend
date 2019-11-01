214968a848a082f2c125567bcbb1e10edd84e22c
oliburt <oliburt@gmail.com>
2019-11-01 13:31:32 +0000

    update method added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,19 @@ class Api::V1::UsersController < ApplicationController
         end
     end
 
+    def update
+        user = User.find(params[:id])
+        if user.authenticate(user_params[:password])
+            if user.update(first_name: user_params[:first_name], last_name: user_params[:last_name], email: user_params[:email])
+                render json: user
+            else
+                render json: { errors: user.errors.full_messages }, status: :not_acceptable
+            end
+        else
+            render json: { errors: ['Incorrect Password'] }, status: :not_acceptable
+        end
+    end
+
     private
 
     def user_params

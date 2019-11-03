fa8a632c3d03578ebe01c445262f280cc9064a88
oliburt <oliburt@gmail.com>
2019-11-03 15:41:27 +0000

    strong params and venue create action

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -8,6 +8,18 @@ class Api::V1::VenuesController < ApplicationController
     end
 
     def create
-        
+        venue = Venue.create(venue_params)
+        venue.update(user: @current_user)
+        if venue.valid?
+            render json: venue
+        else
+            render json: { errors: venue.errors.full_messages }, status: :not_accepted
+        end
+    end
+
+    private
+
+    def venue_params
+        params.require(:venue).permit(:name, :description, :place_id, :latitude, :longitude)
     end
 end

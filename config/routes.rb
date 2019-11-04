25d04daeb73c25cd01d564013785148f6a0a05ef
oliburt <oliburt@gmail.com>
2019-11-04 15:04:58 +0000

    routes for categories index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
     namespace :v1 do
+      resources :categories, only: [:index]
       resources :users, only: [:index, :update]
       resources :venues, only: [:create, :show, :index]
       post '/login', to: 'auth#create'

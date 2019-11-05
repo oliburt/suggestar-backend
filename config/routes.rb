f8d69f48dce57879635c02e76f9845400f18528e
oliburt <oliburt@gmail.com>
2019-11-05 10:42:35 +0000

    added show for listing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :listings, only: [:index, :create]
+      resources :listings, only: [:index, :show, :create]
       resources :categories, only: [:index]
       resources :users, only: [:index, :update]
       resources :venues, only: [:create, :show, :index]

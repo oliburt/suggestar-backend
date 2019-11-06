ced4d8674244226e1d2c363005a7e6b0176412eb
oliburt <oliburt@gmail.com>
2019-11-06 14:58:50 +0000

    added likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_204458) do
+ActiveRecord::Schema.define(version: 2019_11_06_144829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_11_05_204458) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "listing_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["listing_id"], name: "index_likes_on_listing_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "listing_categories", force: :cascade do |t|
@@ -76,6 +85,8 @@ ActiveRecord::Schema.define(version: 2019_11_05_204458) do
     t.index ["user_id"], name: "index_venues_on_user_id"
   end
 
+  add_foreign_key "likes", "listings"
+  add_foreign_key "likes", "users"
   add_foreign_key "listing_categories", "categories"
   add_foreign_key "listing_categories", "listings"
   add_foreign_key "listings", "venues"

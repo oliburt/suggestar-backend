eb1d21167a51a104bc31edc237a1ca6da9f8d42a
oliburt <oliburt@gmail.com>
2019-11-06 11:25:51 +0000

    serve up venue with only upcoming listings

--- app/serializers/venue_serializer.rb
+++ app/serializers/venue_serializer.rb
@@ -1,6 +1,7 @@
 class VenueSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :place_id, :latitude, :longitude
-  belongs_to :user
-  has_many :listings
-  
+  attributes :id, :name, :description, :place_id, :latitude, :longitude, :current_listings, :user_id, :address
+
+  def current_listings
+    self.object.listings.filter{ |listing| listing.within_correct_time }
+  end
 end

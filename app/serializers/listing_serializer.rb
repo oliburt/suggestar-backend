3b6e5d4ab5d4af5d82358cdb7fd12bd691d94040
oliburt <oliburt@gmail.com>
2019-11-06 14:59:46 +0000

    serialize likes

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,5 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :ticket_url, :age_restriction, :begin_datetime, :end_datetime
+  attributes :id, :title, :description, :ticket_url, :age_restriction, :begin_datetime, :end_datetime, :likes
   has_many :categories
   belongs_to :venue
 end

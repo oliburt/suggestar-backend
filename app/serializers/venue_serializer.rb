0f9b8b481761563a2eefd3e1b224020a0bf826e8
oliburt <oliburt@gmail.com>
2019-11-06 14:59:58 +0000

    serialize reviews

--- app/serializers/venue_serializer.rb
+++ app/serializers/venue_serializer.rb
@@ -1,5 +1,5 @@
 class VenueSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :place_id, :latitude, :longitude, :current_listings, :user_id, :address
+  attributes :id, :name, :description, :place_id, :latitude, :longitude, :current_listings, :user_id, :address, :reviews
 
   def current_listings
     self.object.listings.filter{ |listing| listing.within_correct_time }

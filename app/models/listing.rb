b21d88053791945e523cdcaf7806b2c455cf95d4
oliburt <oliburt@gmail.com>
2019-11-06 11:25:27 +0000

    correct time period method added

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,8 @@ class Listing < ApplicationRecord
     belongs_to :venue
     has_many :listing_categories
     has_many :categories, through: :listing_categories
+
+    def within_correct_time
+        ((self.begin_datetime.to_datetime >= DateTime.now && self.begin_datetime.to_datetime < 1.days.from_now ) || (self.begin_datetime.to_datetime < DateTime.now && self.end_datetime.to_datetime >= DateTime.now))
+    end
 end

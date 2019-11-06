da282ef4616810152dd4d13a335c611182af509d
oliburt <oliburt@gmail.com>
2019-11-06 14:59:01 +0000

    has many likes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ApplicationRecord
     belongs_to :venue
     has_many :listing_categories
     has_many :categories, through: :listing_categories
+    has_many :likes
 
     def within_correct_time
         ((self.begin_datetime.to_datetime >= DateTime.now && self.begin_datetime.to_datetime < 1.days.from_now ) || (self.begin_datetime.to_datetime < DateTime.now && self.end_datetime.to_datetime >= DateTime.now))

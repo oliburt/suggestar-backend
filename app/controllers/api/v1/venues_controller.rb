c5efd9522ed649a957dc1129089a882456eb2975
oliburt <oliburt@gmail.com>
2019-11-06 11:25:11 +0000

    update and destroy

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -17,9 +17,30 @@ class Api::V1::VenuesController < ApplicationController
         end
     end
 
+    def update
+        venue = Venue.find(params[:id])
+        # byebug
+        venue.update(venue_params)
+        
+        if venue.valid?
+            render json: venue
+        else
+            render json: { errors: venue.errors.full_messages }, status: :not_accepted
+        end
+    end
+
+    def destroy
+        venue = Venue.find(params[:id])
+        if venue.destroy
+            render json: venue
+        else
+            render json: { errors: venue.errors.full_messages }, status: :not_accepted
+        end
+    end
+
     private
 
     def venue_params
-        params.require(:venue).permit(:name, :description, :place_id, :latitude, :longitude)
+        params.require(:venue).permit(:name, :description, :place_id, :latitude, :longitude, :address, :id)
     end
 end

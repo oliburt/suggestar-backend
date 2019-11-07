72cca2834d91fbd071fcfb1db517aabd30089b10
oliburt <oliburt@gmail.com>
2019-11-07 19:27:55 +0000

    changed within_correct_time to serve all future listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,6 @@ class Listing < ApplicationRecord
     has_many :likes
 
     def within_correct_time
-        ((self.begin_datetime.to_datetime >= DateTime.now && self.begin_datetime.to_datetime < 1.days.from_now ) || (self.begin_datetime.to_datetime < DateTime.now && self.end_datetime.to_datetime >= DateTime.now))
+        self.begin_datetime.to_datetime >= DateTime.now || (self.begin_datetime.to_datetime < DateTime.now && self.end_datetime.to_datetime >= DateTime.now)
     end
 end

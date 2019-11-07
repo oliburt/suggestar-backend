ee2e72da9bb3d5409cf8797edf41f80ff93f319b
oliburt <oliburt@gmail.com>
2019-11-07 19:31:16 +0000

    removed listings for SSOT

--- app/serializers/venue_serializer.rb
+++ app/serializers/venue_serializer.rb
@@ -1,7 +1,4 @@
 class VenueSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :place_id, :latitude, :longitude, :current_listings, :user_id, :address, :reviews
+  attributes :id, :name, :description, :place_id, :latitude, :longitude, :user_id, :address, :reviews
 
-  def current_listings
-    self.object.listings.filter{ |listing| listing.within_correct_time }
-  end
 end

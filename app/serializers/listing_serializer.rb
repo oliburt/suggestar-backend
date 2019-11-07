c7b2833c5243ff054cdb0aff7a4d4d1c5ebc5f14
oliburt <oliburt@gmail.com>
2019-11-07 19:28:39 +0000

    venue_id and user_id for SSOT on front end

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,10 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :ticket_url, :age_restriction, :begin_datetime, :end_datetime, :likes
+  attributes :id, :title, :description, :ticket_url, :age_restriction, :begin_datetime, :end_datetime, :likes, :venue_id, :user_id
   has_many :categories
-  belongs_to :venue
+  # belongs_to :venue
+
+  def user_id
+    self.object.venue.user.id
+  end
+
 end

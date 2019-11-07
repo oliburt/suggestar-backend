e65fd2a7a6c6e219a8a1a6e5082733b642e74548
oliburt <oliburt@gmail.com>
2019-11-07 19:29:58 +0000

    venue ids for SSOT on front end

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :full_name
-  has_many :venues
+  attributes :id, :email, :first_name, :last_name, :full_name, :venues
+
+  def venues
+    self.object.venues.map{|v| v.id}
+  end
 end

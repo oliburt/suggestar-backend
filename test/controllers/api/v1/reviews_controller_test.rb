60d968a669a0e89a70684636203c690cd96c156d
oliburt <oliburt@gmail.com>
2019-11-08 11:40:25 +0000

    reviews test file

---
+++ test/controllers/api/v1/reviews_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Api::V1::ReviewsControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get api_v1_reviews_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get api_v1_reviews_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_v1_reviews_destroy_url
+    assert_response :success
+  end
+
+end

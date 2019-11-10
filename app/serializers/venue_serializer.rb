72d49952c47fa21bbe78c4c7e7d390660e1f8a92
oliburt <oliburt@gmail.com>
2019-11-10 10:54:07 +0000

    serve up image_url and image_public_id

--- app/serializers/venue_serializer.rb
+++ app/serializers/venue_serializer.rb
@@ -1,5 +1,5 @@
 class VenueSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :place_id, :latitude, :longitude, :user_id, :address, :reviews
+  attributes :id, :name, :description, :place_id, :latitude, :longitude, :user_id, :address, :reviews, :image_url, :image_public_id
   has_many :reviews
   
 end
